4a89a5c46b070ff3c8c95226b6839687bc02eb30
Michal Bachraty <misko.bachraty@gmail.com>
2023-05-13 21:12:34 -0700

    fix change special schedule

---
+++ mobile/src/utils/useClearSchedulesForDay.ts
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs';
+import {useGetAllSchedulesQuery} from '../generated/graphql';
+import {useEditSchedule} from '../mutationHooks/schedule/editSchedule';
+
+export const useClearSchedulesForDay = () => {
+  const {data: schedules} = useGetAllSchedulesQuery();
+  const [editSchedule] = useEditSchedule();
+  return (date: dayjs.Dayjs) => {
+    schedules?.getAllSchedules.forEach(item => {
+      if (item.dates?.some(item => dayjs(item).isSame(dayjs(date), 'day'))) {
+        const newDates = item.dates.filter(
+          itemDate => !dayjs(itemDate).isSame(date, 'day'),
+        );
+        editSchedule({
+          id: item.id,
+          dates: newDates,
+        });
+      }
+    });
+  };
+};
