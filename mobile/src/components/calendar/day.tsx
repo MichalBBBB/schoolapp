5965b84fa9b7bc5a4469c0b9670e2d81cf78d182
Michal Bachraty <misko.bachraty@gmail.com>
2024-01-08 07:40:54 +0100

    i don't know

--- mobile/src/components/calendar/day.tsx
+++ mobile/src/components/calendar/day.tsx
@@ -1,12 +1,8 @@
 import dayjs from 'dayjs';
 import React, {memo, useContext, useEffect} from 'react';
-<<<<<<< HEAD
-import {TouchableOpacity, View, Text} from 'react-native';
-=======
 import {TouchableOpacity, View, Text, Pressable} from 'react-native';
 import {DayWithDot} from '.';
 import {useTheme} from '../../contexts/ThemeContext';
->>>>>>> bdcc667252e2be39bf69778b1e7af38577bcdb76
 import {BasicText} from '../basicViews/BasicText';
 
 interface DayProps {
@@ -68,22 +64,9 @@ const Day: React.FC<DayProps> = ({
 };
 
 export default memo(Day, (prevProps, nextProps) => {
-<<<<<<< HEAD
   if (prevProps.isSelected == nextProps.isSelected) {
     return true;
   } else {
     return false;
   }
-=======
-  if (prevProps.day.dot !== nextProps.day.dot) {
-    return false;
-  }
-  if (prevProps.isSelected !== nextProps.isSelected) {
-    return false;
-  }
-  if (!prevProps.day.date.isSame(nextProps.day.date)) {
-    return false;
-  }
-  return true;
->>>>>>> bdcc667252e2be39bf69778b1e7af38577bcdb76
 });
