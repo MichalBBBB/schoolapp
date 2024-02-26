0134010520492510830dbc53636389ea65e0b67a
MichalBBBB <misko.bachraty@gmail.com>
2024-02-26 08:29:37 +0100

    paywall design change

--- mobile/src/contexts/PremiumWindowContext.tsx
+++ mobile/src/contexts/PremiumWindowContext.tsx
@@ -1,7 +1,7 @@
 import {useReactiveVar} from '@apollo/client';
 import React, {useEffect} from 'react';
 import {createContext, useContext, useState} from 'react';
-import {View} from 'react-native';
+import {ImageSourcePropType, View} from 'react-native';
 import Purchases, {PurchasesPackage} from 'react-native-purchases';
 import {isLoadingVar} from '../App';
 import {BasicButton} from '../components/basicViews/BasicButton';
@@ -9,12 +9,44 @@ import {BasicModalCard} from '../components/basicViews/BasicModalCard';
 import {BasicText} from '../components/basicViews/BasicText';
 import {packagesVar} from '../Content';
 import {refreshToken} from '../utils/AccessToken';
+import {BasicIcon} from '../components/basicViews/BasicIcon';
 
 const PremiumWindowContext = createContext<() => void>(() => {});
 
 export const usePremiumWindow = () => {
   const value = useContext(PremiumWindowContext);
   return value;
+};
+
+interface PremiumItemProps {
+  text: string;
+  subtext: string;
+  iconSrc: ImageSourcePropType;
+}
+
+export const PremiumItem: React.FC<PremiumItemProps> = ({
+  text,
+  subtext,
+  iconSrc,
+}) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+      }}>
+      <BasicIcon
+        source={iconSrc}
+        style={{height: 30, width: 30, marginRight: 0}}
+        color="text"
+      />
+      <View style={{flex: 1, backgroundColor: undefined, marginLeft: 10}}>
+        <BasicText style={{fontSize: 18, fontWeight: 'bold'}}>{text}</BasicText>
+        <BasicText color="textSecondary">{subtext}</BasicText>
+      </View>
+    </View>
+  );
 };
 
 export const PremiumWindowProvider: React.FC<{}> = ({children}) => {
@@ -63,21 +95,31 @@ export const PremiumWindowProvider: React.FC<{}> = ({children}) => {
             style={{textAlign: 'center', marginVertical: 10}}>
             Try Premium!
           </BasicText>
-          <View style={{margin: 10, marginLeft: 25}}>
-            <BasicText style={{marginBottom: 5}}>
-              Add unlimited number of tasks
-            </BasicText>
-            <BasicText style={{marginBottom: 5}}>
-              Create group projects
-            </BasicText>
-            <BasicText style={{marginBottom: 5}}>
-              Get advanced timetable options
-            </BasicText>
-            <BasicText style={{marginBottom: 5}}>Add more schedules</BasicText>
+          <View style={{margin: 10, marginLeft: 10}}>
+            <PremiumItem
+              text="More Tasks"
+              subtext="Add unlimited number of tasks"
+              iconSrc={require('../../assets/Tasks.png')}
+            />
+            <PremiumItem
+              text="Group Projects"
+              subtext="Create group projects and invite your friends"
+              iconSrc={require('../../assets/Projects.png')}
+            />
+            <PremiumItem
+              text="Advanced timetable options"
+              subtext="Change rotation length or whether to skip weekends"
+              iconSrc={require('../../assets/Settings.png')}
+            />
+            <PremiumItem
+              text="Add more schedules"
+              subtext="Add more schedules for more complex timetables"
+              iconSrc={require('../../assets/Calendar.png')}
+            />
           </View>
           <View
             style={{
-              flexDirection: 'row',
+              flexDirection: 'row-reverse',
               width: '100%',
             }}>
             {packages.map((item, index) => (
@@ -88,18 +130,42 @@ export const PremiumWindowProvider: React.FC<{}> = ({children}) => {
                 }}
                 borderWidth={1}
                 style={{flex: 1, marginHorizontal: 4}}
-                variant="outlined">
+                backgroundColor={
+                  item.product.subscriptionPeriod == 'P1Y'
+                    ? 'accentBackground'
+                    : undefined
+                }
+                borderColor={
+                  item.product.subscriptionPeriod == 'P1Y'
+                    ? 'accent'
+                    : undefined
+                }
+                variant={
+                  item.product.subscriptionPeriod == 'P1Y'
+                    ? 'outlined'
+                    : 'outlined'
+                }>
                 <BasicText
                   style={{textAlign: 'center'}}
-                  textVariant="subHeading">
+                  textVariant="subHeading"
+                  color={
+                    item.product.subscriptionPeriod == 'P1Y' ? 'accent' : 'text'
+                  }>
                   {item.product.title}
                 </BasicText>
-                <BasicText style={{textAlign: 'center'}}>
+                <BasicText
+                  style={{textAlign: 'center'}}
+                  color={
+                    item.product.subscriptionPeriod == 'P1Y' ? 'accent' : 'text'
+                  }>
                   2 weeks free
                 </BasicText>
                 <BasicText
                   style={{textAlign: 'center'}}
-                  textVariant={'heading'}>
+                  textVariant={'heading'}
+                  color={
+                    item.product.subscriptionPeriod == 'P1Y' ? 'accent' : 'text'
+                  }>
                   {item.product.priceString}
                 </BasicText>
               </BasicButton>
